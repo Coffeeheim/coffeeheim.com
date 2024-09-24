903bae7a0e7500417eadc168822b758e53381503
Sérgio Rafael Siqueira <osergiosiqueira@gmail.com>
2024-09-24 13:27:16 -0300

    add wrangler:

--- functions/permittedlist.js
+++ functions/permittedlist.js
@@ -1,7 +1,24 @@
-export function onRequest(context) {
-  if (context.request.method !== "POST") {
-    return new Response("Method Not Allowed", { status: 405 });
+async function processReq() {
+  await new Promise((r) => setTimeout(r, 2000))
+
+  try {
+    await fetch('https://api.coffeeheim.com/permittedlist', {
+      method: 'POST',
+      body: JSON.stringify({ streamid64: 'example' }),
+    })
+  } catch (err) {
+    console.error('Error fetching data:', err)
+  }
+}
+
+async function onRequest(context) {
+  if (context.request.method !== 'POST') {
+    return new Response('Method Not Allowed', { status: 405 })
   }
 
-  return new Response(JSON.stringify({ hello: "world" }), { status: 200 });
+  processReq()
+
+  return new Response(JSON.stringify({}), { status: 202 })
 }
+
+export default onRequest
